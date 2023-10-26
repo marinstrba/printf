117e1cccfb19feea9f30c900c27021c936670c87
Marian Strba <mstrba@c1r8s4.42prague.com>
2023-10-26 16:37:52 +0200

    pls

---
+++ ft_phexl.c
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_phexl.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mstrba <mstrba@student.42prague.com>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/10/25 17:21:06 by mstrba            #+#    #+#             */
+/*   Updated: 2023/10/26 15:48:33 by mstrba           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libftprintf.h"
+
+int	ft_phexl(va_list args)
+{
+	unsigned int	n;
+	int				res;
+
+	n = va_arg(args, unsigned int);
+	res = ft_puthexlu(n, 1);
+	return (res);
+}
+
